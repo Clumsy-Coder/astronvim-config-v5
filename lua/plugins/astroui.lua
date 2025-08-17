c10e7df0f7e8c2a1686fd67815070f677be05105
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-08-16 18:17:35 -0600

    build(plugins:astroui): add plugin configs
      ## what
      - add plugin configs
        - add icons
    
      ## how
      - obtained from
        - https://github.com/Clumsy-Coder/astronvim-config-v4/blob/5e49fff/lua/plugins/astroui.lua#L24-L58
    
      ## why
    
      ## where
      - ./lua/plugins/astroui.lua
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -22,6 +22,17 @@ return {
       },
     },
     -- Icons can be configured throughout the interface
+    -- obtained from:
+    --    https://github.com/datamonsterr/astronvim_config/blob/67bdd62/icons.lua
+    --    https://github.com/AstroNvim/AstroNvim/blob/0fee587489/lua/configs/cmp.lua
+    --
+    -- https://github.com/microsoft/vscode/blob/main/src/vs/base/common/codicons.ts
+    -- https://github.com/microsoft/vscode/blob/84ba0e1/src/vs/base/common/codicons.ts
+    -- https://jdhao.github.io/2020/10/07/nvim_insert_unicode_char/#use-ctrl-v-to-insert-character
+    -- go to the above and then enter <c-v>u<unicode or hexcode> and the symbold should appear
+    -- or go here and upload the font file: https://mathew-kurian.github.io/CharacterMap/
+    -- find more here: https://www.nerdfonts.com/cheat-sheet
+    --   פּ ﯟ   蘒練 some other good icons
     icons = {
       -- configure the loading of the lsp in the status line
       LSPLoading1 = "⠋",
@@ -34,6 +45,16 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
+      -- used by plugin `folke/todo-comments.nvim`
+      TodoDone = " ",
+      TodoFix = " ",
+      TodoHack = " ",
+      TodoNote = " ",
+      TodoPerformance = " ",
+      -- TodoTest = "ﭧ ",
+      TodoTest = " ",
+      Todo = " ",
+      TodoWarn = " ",
     },
   },
 }

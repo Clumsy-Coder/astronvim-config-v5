7ab7951f77abcafc578b1ca41b8874213df1944c
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-08-18 12:56:36 -0600

    build(community:recipes): add plugin `astrocommunity.recipes.heirline-nvchad-statusline`
      ## what
      - add plugin `astrocommunity.recipes.heirline-nvchad-statusline`
    
      ## how
      - obtained from
        - https://github.com/AstroNvim/astrocommunity/tree/fd3bac1/lua/astrocommunity/recipes/heirline-nvchad-statusline
    
      ## why
    
      ## where
      - ./lua/community.lua
    
      ## usage
    
      ## commit id, issue or pull request
    
      ## notes

--- lua/community.lua
+++ lua/community.lua
@@ -52,6 +52,7 @@ return {
 
   -- ------------------------------------------------------------------------------------------- --
   -- recipe
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },     -- NVchad statusline theme
   { import = "astrocommunity.recipes.heirline-tabline-buffer-number" }, -- show buffer number before the filename
 
   -- ------------------------------------------------------------------------------------------- --

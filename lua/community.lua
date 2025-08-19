35c4b7a02fe4b6b55d789cb7d0d6dd1ceed7588f
Clumsy-Coder <19594044+Clumsy-Coder@users.noreply.github.com>
2025-08-19 09:36:48 -0600

    chore(community): remove duplicate `astrocommunity.pack.lua` entry

--- lua/community.lua
+++ lua/community.lua
@@ -52,7 +52,6 @@ return {
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.tailwindcss" },
